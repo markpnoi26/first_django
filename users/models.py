82766439c259e3595d6533dbc58dd61a99ee802a
markpnoi26 <markpnoi26@gmail.com>
2020-03-06 13:44:20 -0800

    added Profile model fields

--- users/models.py
+++ users/models.py
@@ -7,6 +7,8 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    header = models.CharField(max_length=255, blank=True)
+    about_me = models.TextField(blank=True)
 
     def __str__(self):
         return f"{self.user.username} Profile"
